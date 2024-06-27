ce406df01bdcecbea0f89f4efb64d439a05a6375
d116626 <d116626@gmail.com>
2024-06-27 10:51:54 -0300

    feat: add dbt pipeline

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -4,3 +4,4 @@ Imports all flows for every project so we can register all of them
 """
 from pipelines.conservacao_ambiental import *  # noqa
 from pipelines.google_earth_engine import *  # noqa
+from pipelines.templates import *  # noqa
