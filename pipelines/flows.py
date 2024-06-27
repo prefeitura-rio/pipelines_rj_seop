e1b0c0028c3e1f49d7c628c95c7908b037385918
d116626 <d116626@gmail.com>
2024-06-27 10:37:40 -0300

    fix: wrong parameter name in datframe_to_csv

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Imports all flows for every project so we can register all of them.
+Imports all flows for every project so we can register all of them
 """
 from pipelines.conservacao_ambiental import *  # noqa
 from pipelines.google_earth_engine import *  # noqa
